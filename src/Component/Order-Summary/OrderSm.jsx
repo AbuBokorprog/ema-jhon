7ed3a1a4cadac12a47f133f8977ea0ed5e84ba40
Abu Bokor <abubokor1066@gmail.com>
2023-03-28 22:44:56 +0600

    show data from local storage

--- src/Component/Order-Summary/OrderSm.jsx
+++ src/Component/Order-Summary/OrderSm.jsx
@@ -3,13 +3,19 @@ import Cart from '../Cart/Cart';
 
 const OrderSm = (props) => {
     const [cart] = [props.cart]
-    console.log(cart);
+    //console.log(cart);
     let total = 0;
     let totalShipping= 0;
+    let quantity = 0;
     for(let product of cart){
-        total += product.price;
-        totalShipping += product.shipping
-        //console.log(product);
+        /* if(product.quantity == 0){
+            product.quantity = 1;
+        } */
+        //product.quantity = product.quantity || 1;
+        total += product.price*product.quantity;
+        totalShipping += product.shipping * product.quantity
+        quantity = product.quantity
+        console.log(quantity);
     }
     const tax = total * 5 /100
     const grandTotal = total + totalShipping + tax
@@ -17,7 +23,7 @@ const OrderSm = (props) => {
     return (
         <div className='sticky'>
             <p className='text-xl font-semibold py-5 text-center'>Order Summary</p>
-            <p className='font-medium pl-2 pb-5'>Selected items: {cart.length}</p>
+            <p className='font-medium pl-2 pb-5'>Selected items: {quantity}</p>
             <p className='pl-2 pb-3'>Total Price:{total}</p>
             <p className='pl-2 pb-3'>Total Shipping Charge:{totalShipping} </p>
             <p className='pl-2 pb-3'>Tax:{tax.toFixed(2)}</p>
